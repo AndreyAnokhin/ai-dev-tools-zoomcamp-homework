dae994c0b52ade46594ee719c03f4c6dc94e2c26
Andrei Anokhin <anokhin.andrey@gmail.com>
2025-11-22 21:19:13 +0100

    add templates and views

--- core/views.py
+++ core/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import TodoItem
+
+
+class HomeView(ListView):
+    """Display all TODO items on the home page."""
+    model = TodoItem
+    template_name = 'home.html'
+    context_object_name = 'todos'
+    ordering = ['-created_at']
